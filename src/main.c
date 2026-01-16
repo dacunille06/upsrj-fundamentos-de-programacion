ae90c7457f973fc70b90c59bfa58945c7c3f2aa0
Daniel Cunillé Rodríguez <dacunille@gmail.com>
2026-01-16 11:49:44 -0600

    primera entrega cunillé

--- src/main.c
+++ src/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
-int main(int argc, char *argv)
-{
-    printf("Hello world!\n");
+// 1. **Calculate the area of a square** given the length of its side.
+// 2. **Calculate the volume of a cube** given the length of its side.
+
+float c_area(float side) {
+    float ar = side * side;
+    return ar;
+}
+
+float c_volume(float side) {
+    float vol = side * side * side;
+    return vol;
+}
+
+int main() {
+
+    float side, area, volume; 
+    
+    printf("Enter the side lenght: ");
+    scanf("%f", &side);
+
+    area = c_area(side);
+    volume = c_volume(side);
+
+    printf("Square area: %.2f\n", area);
+    printf("Cube volume: %.2f\n", volume);
+
     return 0;
 }
